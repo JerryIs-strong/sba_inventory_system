53ac990dfec9714ae3401c47125a615969a8eb0a
JerryIs-strong <heyajerry@gmail.com>
2025-08-17 14:38:17 +0800

    Integrity verify

--- app.py
+++ app.py
@@ -19,14 +19,17 @@ version_info = itg.sysInfo()
 @app.route('/', methods=['GET'])
 def home():
     from_admin = session.get("group") == "Admin"
-    if session.get("username"):  
-        if 'page' in request.args:
-            page = request.args['page']
-            return render_template("index.html", version=version_info, username=session["username"], from_admin=from_admin, re_dir=page)
+    if session.get("integrity"):
+        if session.get("username"):  
+            if 'page' in request.args:
+                page = request.args['page']
+                return render_template("index.html", version=version_info, username=session["username"], from_admin=from_admin, re_dir=page)
+            else:
+                return render_template("index.html", version=version_info, username=session["username"], from_admin=from_admin)
         else:
-            return render_template("index.html", version=version_info, username=session["username"], from_admin=from_admin)
+            return redirect(url_for('login'))
     else:
-        return redirect(url_for('login'))
+        return redirect(url_for('suspend'))
     
 @app.route('/dashboard')
 def dashboard():
@@ -78,14 +81,20 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-
         if um.logIn(username, password):
             session["username"] = username
             session["group"] = um.getUserGroup(username)
-            return redirect(url_for('home'))
+            if itg.verifyMD5():
+                itg.log(f'[{session["username"]}] User "{username}" success to sign-in system')
+                return redirect(url_for('home'))
+            else:
+                flash("Database integrity not pass. System ended", 'error')
+                itg.log(f'[{session["username"]}] system quit due to database integrity test fail')
+                return redirect(url_for('suspend'))
         else:
-            flash('Username/Password incorrect. Please try again')
-    
+            itg.log(f'[Guest] User "{username}" try to signin into system')
+            flash('Username/Password incorrect. Please try again', 'error')
+        return redirect(url_for('login'))
     return render_template('login.html', version=version_info)
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -139,6 +148,23 @@ def update_account(type):
 def logout():
     session.clear()
     return redirect(url_for('login'))
+
+@app.route('/suspend')
+def suspend():
+    session.clear()
+    session["integrity"] = False
+    return render_template("danger.html")
+
+@app.route('/system/integrity/unlock')
+def unlock():
+    if itg.verifyMD5():
+        session["integrity"] = True
+        itg.log(f'[Admin] System unlocked')
+        return redirect(url_for('home'))
+    else:
+        print("unlock fail")
+        return redirect(url_for('suspend'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
